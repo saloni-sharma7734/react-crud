1235705aebf6166a096d9374a6d8981dccf58d7a
unknown <vidhikh2001@gmail.com>
2023-10-03 19:11:16 +0530

    done

--- src/App.js
+++ src/App.js
@@ -3,18 +3,20 @@ import "./App.css";
 import Login from "./components/login";
 import Home from "./components/Home";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Profile from './components/pages/Profile.jsx';
+
 
 function App() {
   return (
  
       <BrowserRouter>
       <Routes>
-     
-        <Route exact path="" element={<Login/>} />
+      <>
+        <Route path="" element={<Login/>} />
         <Route path="/login" element={<Login/>} />
         <Route path="/home" element={<Home/>} />
-        <p>Hello</p>
-      
+        <Route path="/profile" element={<Profile/>}/>
+        </>
     </Routes>
     </BrowserRouter>
    
