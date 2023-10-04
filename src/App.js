b0e43286efa4d8d698ca77c65e07d919175b34ff
i-am-dheeraj <dheerajdhingra76@gmail.com>
2023-10-04 10:47:44 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from "./logo.svg";
+// import logo from './logo.svg';
 import "./App.css";
-import Login from "./components/login";
-import Home from "./components/Home";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
- 
-      <BrowserRouter>
-      <Routes>
-     
-        <Route exact path="" element={<Login/>} />
-        <Route path="/login" element={<Login/>} />
-        <Route path="/home" element={<Home/>} />
-        <p>Hello</p>
-      
-    </Routes>
-    </BrowserRouter>
-   
+    <>
+      <div className="container-fluid">
+        <Sidebar />
+        <button className="btn btn-primary">Logout</button>
+      </div>
+    </>
   );
 }
 
