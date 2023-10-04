22d97a72fb2beaf3f475712e5e2990d46776efec
saloni <sharmasaloni661@gmail.com>
2023-10-04 16:36:44 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-// import logo from "./logo.svg";
+import logo from "./logo.svg";
 import "./App.css";
-// import Login from "./components/login";
-// import Home from "./components/Home";
-// import Navbar from "./components/Navbar";
-// import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./components/login";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
-  // return (
+  return (
  
-  //     <BrowserRouter>
-  //     <Routes>
+      <BrowserRouter>
+      <Routes>
      
-  //       <Route exact path="" element={<Login/>} />
-  //       <Route path="/login" element={<Login/>} />
-  //       <Route path="/home" element={<Home/>} />
-  //       <Route path="/Navbar" element={<Navbar/>}/>
+        <Route exact path="" element={<Login/>} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/home" element={<Home/>} />
+        <Route path="/Navbar" element={<Navbar/>}/>
       
-  //   </Routes>
-  //   </BrowserRouter>
+    </Routes>
+    </BrowserRouter>
    
-  // );
+  );
 }
 
 export default App;
